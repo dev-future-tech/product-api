8be6b4491dfcded0341754404780c445b3d45bd4
Anthony Ikeda <anthony.ikeda@gmail.com>
2022-02-11 20:56:18 -0800

    Code Cleanup

--- src/main/java/org/flower/productapi/ProductController.java
+++ src/main/java/org/flower/productapi/ProductController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
