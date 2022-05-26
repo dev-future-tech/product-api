ceb3ebab8f17a3db2396e77148947230da243162
Anthony Ikeda <anthony.ikeda@gmail.com>
2022-05-26 11:50:15 -0700

    Limiting the message listener to profile test

--- src/main/java/org/flower/productapi/ProductRequestValidatedConsumer.java
+++ src/main/java/org/flower/productapi/ProductRequestValidatedConsumer.java
@@ -3,9 +3,11 @@ package org.flower.productapi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile(value = {"!test"})
 public class ProductRequestValidatedConsumer {
 
     private Logger log = LoggerFactory.getLogger(ProductRequestValidatedConsumer.class);
