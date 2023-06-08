1d6bfd2cad2b4649c4a2347f8af3d9281b5d75aa
Anthony Ikeda <anthony.ikeda@gmail.com>
2023-06-08 10:27:54 -0700

    Added new model

---
+++ src/main/java/org/flower/productapi/ProductAdvertisement.java
@@ -0,0 +1,23 @@
+package org.flower.productapi;
+
+public class ProductAdvertisement {
+
+    private String message;
+    private String headline;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+}
